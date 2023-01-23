158b8c0886ce58f0ac3c1a0316c6055511ff0e76
BenjaminLeveille <blveill@icloud.com>
2023-01-23 13:27:11 -0500

    route pour vue jeu solo

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -4,6 +4,7 @@ import { GameCreationPageComponent } from '@app/pages/game-creation-page/game-cr
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { SoloViewPageComponent } from '@app/pages/solo-view-page/solo-view-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: 'gameCreation', component: GameCreationPageComponent },
+    { path: 'soloView', component: SoloViewPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
