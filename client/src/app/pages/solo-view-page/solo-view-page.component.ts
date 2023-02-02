41f3f7cb984b27d03ddd1138c9d11976734c4cb6
SelimBellagha <ms.bellagha@gmail.com>
2023-02-01 23:47:12 -0500

    feature/timer

--- client/src/app/pages/solo-view-page/solo-view-page.component.ts
+++ client/src/app/pages/solo-view-page/solo-view-page.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-solo-view-page',
     templateUrl: './solo-view-page.component.html',
     styleUrls: ['./solo-view-page.component.scss'],
 })
-export class SoloViewPageComponent {}
+export class SoloViewPageComponent implements OnInit {
+    minutes: number = 0;
+    secondes1: number = 0;
+    secondes2: number = 0;
+    minutes1: number = 0;
+    minutes2: number = 0;
+
+    timer() {
+        const decimalMax = 9;
+        const centaineMax = 5;
+        const timerInterval = 1000;
+        setInterval(() => {
+            if (this.secondes2 === centaineMax && this.secondes1 === decimalMax) {
+                this.secondes2 = 0;
+                this.secondes1 = 0;
+                this.minutes1++;
+            } else if (this.minutes1 === decimalMax) {
+                this.minutes1 = 0;
+                this.minutes2++;
+            }
+            if (this.secondes1 === decimalMax) {
+                this.secondes2++;
+                this.secondes1 = 0;
+            } else {
+                this.secondes1++;
+            }
+        }, timerInterval);
+    }
+
+    ngOnInit() {
+        this.startTimer();
+    }
+
+    startTimer() {
+        this.timer();
+    }
+}
