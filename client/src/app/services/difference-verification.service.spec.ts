8415b0a6d41120116e3c09c0d94250a5940097ee
SelimBellagha <ms.bellagha@gmail.com>
2023-02-13 11:00:33 -0500

    fix test

--- client/src/app/services/difference-verification.service.spec.ts
+++ client/src/app/services/difference-verification.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { DifferenceVerificationService } from './difference-verification.service';
@@ -6,7 +7,9 @@ describe('DifferenceVerificationService', () => {
     let service: DifferenceVerificationService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
         service = TestBed.inject(DifferenceVerificationService);
     });
 
--- client/src/app/services/difference-verification.service.ts
+++ client/src/app/services/difference-verification.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Message } from '@common/message';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 @Injectable({
     providedIn: 'root',
@@ -13,7 +13,7 @@ export class DifferenceVerificationService {
 
     async differenceVerification(clickX: number, clickY: number, id: number) {
         const params = new HttpParams().set('ClickX', clickX).set('ClickY', clickY);
-        this.http.get<Message>(this.baseUrl + `/difference/${id}`, { params }).subscribe((res) => console.log(res));
+        this.http.get<Message>(this.baseUrl + `/difference/${id}`, { params }).pipe(catchError(this.handleError<Message>('differenceVerification')));
     }
 
     private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
