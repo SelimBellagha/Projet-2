9d78013dd2445b2a780b4ee10cb624b3d6473577
Asimina Koutsos <97801892+asiminakoutsos@users.noreply.github.com>
2023-02-14 20:34:56 -0500

    tests serveur fini

--- server/app/controllers/games.controller.spec.ts
+++ server/app/controllers/games.controller.spec.ts
@@ -1,22 +1,18 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-require-imports */
-/* eslint-disable @typescript-eslint/naming-convention */
 import { Application } from '@app/app';
 import { GameData } from '@app/data/game.interface';
 import { GameManager } from '@app/services/game-manager.service';
+import { expect } from 'chai';
 import { StatusCodes } from 'http-status-codes';
 import { createStubInstance, SinonStubbedInstance } from 'sinon';
 import * as supertest from 'supertest';
 import { Container } from 'typedi';
-// import sinon = require('sinon');
 
 const HTTP_STATUS_OK = StatusCodes.OK;
 const HTTP_STATUS_CREATED = StatusCodes.CREATED;
 const HTTP_SERVER_ERROR = StatusCodes.INTERNAL_SERVER_ERROR;
 const HTTP_NOT_FOUND = StatusCodes.NOT_FOUND;
 
-describe('GameController', async () => {
-    let gameService: SinonStubbedInstance<GameManager>;
+describe('GameController', () => {
     const gamesData = [
         {
             id: '0',
@@ -37,17 +33,36 @@ describe('GameController', async () => {
             isDifficult: false,
         },
     ] as GameData[];
+    const game = {
+        id: '1',
+        name: 'testGame',
+        originalImage: '',
+        modifiedImage: '',
+        nbDifferences: 8,
+        differences: [],
+        isDifficult: false,
+    } as GameData;
+    let gameService: SinonStubbedInstance<GameManager>;
     let expressApp: Express.Application;
 
     beforeEach(async () => {
         gameService = createStubInstance(GameManager);
+        gameService.getAllGames.resolves(gamesData);
+        gameService.addGame.resolves(game);
         const app = Container.get(Application);
+        Object.defineProperty(app['gamesController'], 'gameService', { value: gameService });
         expressApp = app.app;
     });
 
     it('should return an array of games on valid get request to /games', async () => {
-        gameService.getAllGames.resolves(gamesData);
-        return supertest(expressApp).get('/api/games/').send(gamesData).set('Accept', 'application/json').expect(HTTP_STATUS_OK);
+        return supertest(expressApp)
+            .get('/api/games/')
+            .send(gamesData)
+            .set('Accept', 'application/json')
+            .expect(HTTP_STATUS_OK)
+            .then((response) => {
+                expect(response.body).to.deep.equal(gamesData);
+            });
     });
 
     it('should return 500 error on invalid get request to /games', async () => {
@@ -57,8 +72,15 @@ describe('GameController', async () => {
     });
 
     it('should return game from game service on valid get request to root', async () => {
-        gameService.getGamebyId.withArgs('1').resolves(gamesData[1]);
-        return supertest(expressApp).get('/api/games/1').send(gamesData[1]).set('Accept', 'application/json').expect(HTTP_STATUS_OK);
+        gameService.getGamebyId.withArgs('0').resolves(gamesData[0]);
+        return supertest(expressApp)
+            .get('/api/games/0')
+            .send(gamesData[0])
+            .set('Accept', 'application/json')
+            .expect(HTTP_STATUS_OK)
+            .then((response) => {
+                expect(response.body).to.deep.equal(gamesData[0]);
+            });
     });
 
     it('should return 404 not found if game not found', async () => {
@@ -73,30 +95,12 @@ describe('GameController', async () => {
     });
 
     it('should store game in the array on valid post request to /send', async () => {
-        const game: GameData = {
-            id: '1',
-            name: 'testGame',
-            originalImage: '',
-            modifiedImage: '',
-            nbDifferences: 8,
-            differences: [],
-            isDifficult: false,
-        };
         return supertest(expressApp).post('/api/games/send').send(game).set('Accept', 'application/json').expect(HTTP_STATUS_CREATED);
     });
 
     it('should return 500 error on invalid post request to /send', async () => {
-        const game: GameData = {
-            id: '1',
-            name: 'testGame',
-            originalImage: '',
-            modifiedImage: '',
-            nbDifferences: 8,
-            differences: [],
-            isDifficult: false,
-        };
         const error = new Error('service error');
-        gameService.addGame.withArgs(game).throws(error);
+        gameService.addGame.throws(error);
         return supertest(expressApp).post('/api/games/send').send(error.message).expect(HTTP_SERVER_ERROR);
     });
 });
