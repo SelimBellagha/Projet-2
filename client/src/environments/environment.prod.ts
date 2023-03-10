015b7db87fc40d1a01407d220d0bafd76141a63c
Asimina Koutsos <97801892+asiminakoutsos@users.noreply.github.com>
2023-03-10 13:27:07 -0500

    modif server url in enviroment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'ec2-35-183-155-3.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-35-183-155-3.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
 };
