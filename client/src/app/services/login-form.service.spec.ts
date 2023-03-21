dfc13c3e1fa25a2a442fdd18f43ded95d0ac6ee6
RPzdc259 <debanjan.bhaumick@intact.net>
2023-03-21 13:37:14 -0400

    fini tests login-form

--- client/src/app/services/login-form.service.spec.ts
+++ client/src/app/services/login-form.service.spec.ts
@@ -8,6 +8,7 @@ describe('LoginFormService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(LoginFormService);
+        service = new LoginFormService();
     });
 
     it('should be created', () => {
@@ -18,5 +19,36 @@ describe('LoginFormService', () => {
         const name = 'TestName';
         service.setFormData(name);
         expect(service.getFormData()).toEqual(name);
+    });
+
+    describe('getFormData', () => {
+        it('should return the username', () => {
+            const name = 'John';
+            service.setFormData(name);
+            expect(service.getFormData()).toEqual(name);
+        });
+    });
+    it('should set and get game type', () => {
+        const isMultiplayer = true;
+        service.setGameType(isMultiplayer);
+        expect(service.getGameType()).toEqual(isMultiplayer);
+    });
+
+    it('should set and get player type', () => {
+        const isHost = true;
+        service.setPlayerType(isHost);
+        expect(service.getPlayerType()).toEqual(isHost);
+    });
+
+    it('should set and get game ID', () => {
+        const id = '1234';
+        service.setGameId(id);
+        expect(service.getGameId()).toEqual(id);
+    });
+
+    it('should set and get room ID', () => {
+        const id = '5678';
+        service.setRoomId(id);
+        expect(service.getRoomId()).toEqual(id);
     });
 });
