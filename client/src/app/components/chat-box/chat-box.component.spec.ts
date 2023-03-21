dcc437ccf96e217168399de4c72129776af66eb9
yahyabadrane <70984293+yahyabadrane@users.noreply.github.com>
2023-03-21 19:29:08 -0400

    test chat a 80%

--- client/src/app/components/chat-box/chat-box.component.spec.ts
+++ client/src/app/components/chat-box/chat-box.component.spec.ts
@@ -1,12 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { LoginFormService } from '@app/services/login-form.service';
+import { SocketClientService } from '@app/services/socket-client-service.service';
+import { Message } from '@common/chatMessage';
 import { ChatBoxComponent } from './chat-box.component';
 
 describe('ChatBoxComponent', () => {
     let component: ChatBoxComponent;
     let fixture: ComponentFixture<ChatBoxComponent>;
+    let mockActivatedRoute: unknown;
+    let mockSocketService: any;
+    let mockGameUtils: unknown;
+
     beforeEach(async () => {
+        mockActivatedRoute = {
+            snapshot: {
+                routeConfig: {
+                    path: 'test',
+                },
+            },
+        };
+        mockSocketService = jasmine.createSpyObj(['connect', 'on', 'send']);
+        mockGameUtils = jasmine.createSpyObj(['getGameId']);
+
         await TestBed.configureTestingModule({
             declarations: [ChatBoxComponent],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            paramMap: {
+                                get: (key: string) => 'someValue',
+                            },
+                        },
+                    },
+                },
+                { provide: ActivatedRoute, useValue: mockActivatedRoute },
+                { provide: SocketClientService, useValue: mockSocketService },
+                { provide: LoginFormService, useValue: mockGameUtils },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ChatBoxComponent);
@@ -42,5 +75,58 @@ describe('ChatBoxComponent', () => {
         const pattern = /^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
 
         expect(pattern.test(currentTime)).toBe(true);
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should connect to the socket on init', () => {
+        component.ngOnInit();
+        expect(mockSocketService.connect).toHaveBeenCalled();
+    });
+
+    it('should handle chat message sockets', () => {
+        const message: Message = {
+            text: 'Hello',
+            roomId: '123',
+            isSender: false,
+            isSystem: false,
+            name: 'John Doe',
+        };
+        const spy = spyOn(component.messages, 'push');
+
+        mockSocketService.on.and.callFake((event: string, callback: Function) => {
+            if (event === 'receiveChatMessage') {
+                callback(message);
+            }
+        });
+
+        component.ngOnInit();
+
+        expect(mockSocketService.on).toHaveBeenCalledWith('receiveChatMessage', jasmine.any(Function));
+        expect(spy).toHaveBeenCalledWith(message);
+    });
+
+    it('should add a message when calling addMessage', () => {
+        const message: Message = {
+            text: 'Hello',
+            roomId: '123',
+            isSender: true,
+            isSystem: false,
+            name: '',
+        };
+
+        component.message = message.text;
+
+        mockSocketService.send.and.callFake((event: string, data: unknown) => {
+            expect(event).toBe('sendChatToServer');
+            expect(data).toEqual(message);
+        });
+
+        component.addMessage();
+
+        expect(component.messages).toEqual([message]);
+        expect(component.message).toBe('');
     });
 });
