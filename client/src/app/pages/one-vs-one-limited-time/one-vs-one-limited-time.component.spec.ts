e540dc9473bb191860dd8262c7e09e438a09bf46
SelimBellagha <ms.bellagha@gmail.com>
2023-04-11 10:42:22 -0400

    fix pipeline done

--- client/src/app/pages/one-vs-one-limited-time/one-vs-one-limited-time.component.spec.ts
+++ client/src/app/pages/one-vs-one-limited-time/one-vs-one-limited-time.component.spec.ts
@@ -1,23 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OneVsOneLimitedTimeComponent } from './one-vs-one-limited-time.component';
 
 describe('OneVsOneLimitedTimeComponent', () => {
     let component: OneVsOneLimitedTimeComponent;
     let fixture: ComponentFixture<OneVsOneLimitedTimeComponent>;
     // eslint-disable-next-line no-unused-vars
-    let router: Router;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
+            imports: [RouterTestingModule, HttpClientTestingModule],
             declarations: [OneVsOneLimitedTimeComponent],
         }).compileComponents();
 
         fixture = TestBed.createComponent(OneVsOneLimitedTimeComponent);
-        router = TestBed.inject(Router);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
