231ed73a734c28455fe9c8096463aa168f4697ed
RPzdc259 <debanjan.bhaumick@intact.net>
2023-04-15 16:40:41 -0400

    pass les tests

--- client/src/app/components/mode-indice/mode-indice.component.spec.ts
+++ client/src/app/components/mode-indice/mode-indice.component.spec.ts
@@ -1,5 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { ModeIndiceComponent } from './mode-indice.component';
 
 describe('ModeIndiceComponent', () => {
@@ -8,6 +9,7 @@ describe('ModeIndiceComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [RouterTestingModule, HttpClientModule],
             declarations: [ModeIndiceComponent],
         }).compileComponents();
 
