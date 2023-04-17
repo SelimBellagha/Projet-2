7a7e4bc23bc8313355650c5be96d40c77b825ac1
SelimBellagha <ms.bellagha@gmail.com>
2023-04-17 16:20:40 -0400

    test salleAttente and selectionPage done

--- client/src/app/pages/salle-attente/salle-attente.component.spec.ts
+++ client/src/app/pages/salle-attente/salle-attente.component.spec.ts
@@ -2,10 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { SocketTestHelper } from '@app/classes/socket-test-helper';
 import { LobbyService } from '@app/services/lobby.service';
+import { LoginFormService } from '@app/services/login-form.service';
 import { SocketClientService } from '@app/services/socket-client-service.service';
 import { Socket } from 'socket.io-client';
-
 import { SalleAttenteComponent } from './salle-attente.component';
+import SpyObj = jasmine.SpyObj;
 
 class SocketClientServiceMock extends SocketClientService {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -16,21 +17,23 @@ describe('SalleAttenteComponent', () => {
     let component: SalleAttenteComponent;
     let fixture: ComponentFixture<SalleAttenteComponent>;
     let mockRouter: Router;
-    // let mockSocketService: SocketClientService;
     let mockLobbyService: LobbyService;
     let socketHelper: SocketTestHelper;
     let socketServiceMock: SocketClientServiceMock;
+    let loginSpy: SpyObj<LoginFormService>;
 
     beforeEach(async () => {
         socketHelper = new SocketTestHelper();
         socketServiceMock = new SocketClientService();
         socketServiceMock.socket = socketHelper as unknown as Socket;
+        loginSpy = jasmine.createSpyObj('LoginFormService', ['getLimitedTimeGame', 'getFormData', 'getGameId']);
 
         await TestBed.configureTestingModule({
             declarations: [SalleAttenteComponent],
             providers: [
                 { provide: Router, useValue: { navigate: jasmine.createSpy('navigate') } },
                 { provide: SocketClientService, useValue: socketServiceMock },
+                { provide: LoginFormService, useValue: loginSpy },
             ],
         }).compileComponents();
 
@@ -65,6 +68,26 @@ describe('SalleAttenteComponent', () => {
         expect(spy).toHaveBeenCalled();
     });
 
+    it('goToHome should send remove from queue', () => {
+        const spy = spyOn(socketServiceMock, 'send');
+        component.goToHome();
+        expect(spy).toHaveBeenCalledWith('removeFromQueue', {
+            socketId: socketServiceMock.socket.id,
+            gameId: mockLobbyService.roomId,
+        });
+    });
+
+    it('goToHome should navigate to home', () => {
+        component.goToHome();
+        expect(mockRouter.navigate).toHaveBeenCalledWith(['/home']);
+    });
+
+    it('goToHome should disconnect', () => {
+        const spy = spyOn(socketServiceMock, 'disconnect');
+        component.goToHome();
+        expect(spy).toHaveBeenCalled();
+    });
+
     it('if user is host, send from socketClientService should be called', () => {
         mockLobbyService.host = true;
         component.host = true;
@@ -82,9 +105,53 @@ describe('SalleAttenteComponent', () => {
         expect(mockLobbyService.roomId).toEqual('0');
     });
 
-    it('receiving "refused" from socket navigate to gameSelection view', () => {
+    it('receiving "refused" from socket should navigate to gameSelection view', () => {
         component.refuseListen();
         socketHelper.peerSideEmit('refused', { roomId: 0 });
         expect(mockRouter.navigate).toHaveBeenCalled();
+    });
+
+    it('receiving "updateQueue" from socket should update the queue', () => {
+        mockLobbyService.host = true;
+        const newQueueTest = '[["key",{"playerName":"name","socketId":"id"}]]';
+        component.ngOnInit();
+        socketHelper.peerSideEmit('updateQueue', { newQueue: newQueueTest });
+        expect(component.playerQueue.get('key')).toEqual({ playerName: 'name', socketId: 'id' });
+    });
+
+    it('receiving "updateQueue" from socket should update the queue', () => {
+        loginSpy.getLimitedTimeGame.and.returnValue(true);
+        const roomIdTest = '1';
+        const firstGameTest = 1;
+        component.ngOnInit();
+        socketHelper.peerSideEmit('goToCoopGame', { roomId: roomIdTest, firstGame: firstGameTest });
+        expect(mockRouter.navigate).toHaveBeenCalledWith(['/limitedOneVsOne']);
+    });
+
+    it('receiving "updateQueue" from socket should update the queue', () => {
+        const spyRefuse = spyOn(component, 'refuseListen');
+        const spySend = spyOn(socketServiceMock, 'send');
+        loginSpy.getGameId.and.returnValue('gameId');
+        loginSpy.getFormData.and.returnValue('name');
+        component.ngOnInit();
+        expect(spyRefuse).toHaveBeenCalledWith();
+        expect(spySend).toHaveBeenCalledWith('joinQueue', {
+            gameId: 'gameId',
+            playerName: 'name',
+        });
+    });
+
+    it('cancel should call goToHome if limitedGame', () => {
+        const spyHome = spyOn(component, 'goToHome');
+        loginSpy.getLimitedTimeGame.and.returnValue(true);
+        component.cancel();
+        expect(spyHome).toHaveBeenCalled();
+    });
+
+    it('cancel should call goToGameSelection if not limitedGame', () => {
+        const spySelection = spyOn(component, 'goToGameSelection');
+        loginSpy.getLimitedTimeGame.and.returnValue(false);
+        component.cancel();
+        expect(spySelection).toHaveBeenCalled();
     });
 });
