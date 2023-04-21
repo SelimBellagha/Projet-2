7f883da1ed39427ba95551ea0c368ad2430eb215
Asimina Koutsos <97801892+asiminakoutsos@users.noreply.github.com>
2023-04-20 20:48:58 -0400

    history fixed

--- client/src/app/components/victory/victory.component.spec.ts
+++ client/src/app/components/victory/victory.component.spec.ts
@@ -1,21 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MatDialog } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DisplayGameService } from '@app/services/display-game.service';
 import { VictoryComponent } from './victory.component';
 import SpyObj = jasmine.SpyObj;
 describe('VictoryComponent', () => {
     let component: VictoryComponent;
     let fixture: ComponentFixture<VictoryComponent>;
     let matDialogSpy: SpyObj<MatDialog>;
+    let displayServiceSpy: SpyObj<DisplayGameService>;
     let router: Router;
+    const mockDialogData = {
+        data1: {
+            startDate: 'testDate',
+            gameLength: '0:19',
+            gameMode: 'Temps Limite',
+            namePlayer1: 'testName',
+            namePlayer2: '',
+            winnerName: '',
+            nameAbandon: 'testName',
+        },
+        data2: {
+            position: '1',
+            gameId: 'test',
+            gameType: 'test',
+            time: 'test',
+            playerName: 'mock',
+        },
+        data3: false,
+    };
+
     beforeEach(async () => {
         matDialogSpy = jasmine.createSpyObj('MatDialog', ['closeAll']);
+        displayServiceSpy = jasmine.createSpyObj('DisplayGameService', ['checkPlayerScore', 'addHistory']);
         await TestBed.configureTestingModule({
             declarations: [VictoryComponent],
-            imports: [RouterTestingModule],
-            providers: [{ provide: MatDialog, useValue: matDialogSpy }],
+            imports: [RouterTestingModule, HttpClientModule],
+            providers: [
+                { provide: MatDialog, useValue: matDialogSpy },
+                { provide: DisplayGameService, useValue: displayServiceSpy },
+                { provide: MAT_DIALOG_DATA, useValue: mockDialogData },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(VictoryComponent);
@@ -38,5 +66,55 @@ describe('VictoryComponent', () => {
         component.stayForReplay();
         expect(matDialogSpy.closeAll).toHaveBeenCalled();
         expect(spy).toHaveBeenCalledTimes(0);
+    });
+
+    it('should add history and score when data3 is true', () => {
+        component.data = {
+            data1: {
+                startDate: 'testDate',
+                gameLength: '0:19',
+                gameMode: 'Temps Limite',
+                namePlayer1: 'testName',
+                namePlayer2: '',
+                winnerName: '',
+                nameAbandon: 'testName',
+            },
+            data2: {
+                position: '1',
+                gameId: 'test',
+                gameType: 'test',
+                time: 'test',
+                playerName: 'mock',
+            },
+            data3: true,
+        };
+        component.addHistoryAndScore();
+        expect(displayServiceSpy.addHistory).toHaveBeenCalledWith(component.data.data1);
+        expect(displayServiceSpy.checkPlayerScore).toHaveBeenCalledWith(component.data.data2);
+    });
+
+    it('should not add history and score when data3 is false', () => {
+        component.data = {
+            data1: {
+                startDate: 'testDate',
+                gameLength: '0:19',
+                gameMode: 'Temps Limite',
+                namePlayer1: 'testName',
+                namePlayer2: '',
+                winnerName: '',
+                nameAbandon: 'testName',
+            },
+            data2: {
+                position: '1',
+                gameId: 'test',
+                gameType: 'test',
+                time: 'test',
+                playerName: 'mock',
+            },
+            data3: false,
+        };
+        component.addHistoryAndScore();
+        expect(displayServiceSpy.addHistory).not.toHaveBeenCalled();
+        expect(displayServiceSpy.checkPlayerScore).not.toHaveBeenCalled();
     });
 });
